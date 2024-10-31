ccccd39098f376daf22334662022826c5b109f79
Abeeekoala <abraham@LAPTOP-PBSLHUIS>
2024-10-31 14:04:29 +0000

    task1 complete

--- task1/lfsr.sv
+++ task1/lfsr.sv
@@ -4,5 +4,14 @@ module lfsr(
     input   logic       en,
     output  logic [3:0] data_out
 );
+    logic [4:1] sreg;
 
+    always_ff @(posedge clk, posedge rst ) begin
+        if (rst)
+            sreg <= 4'b1;
+        else
+            sreg <= {sreg[3:1], sreg[4] ^ sreg[3]};
+    end
+    
+    assign data_out = sreg;
 endmodule
